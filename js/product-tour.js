58830b70a9d65c7690d6b8569d2ba8449f02d59a
Robophil <oghenerobo@gmail.com>
2016-11-19 16:48:27 +0100

    added helper functions

--- js/product-tour.js
+++ js/product-tour.js
@@ -1,46 +1,61 @@
-/**
- *
- * ### options permitted : attribute(not Used For Now)
- * attribute        |   options     |   Value {default}
- * -----------------|---------------|------------------
- * data-html        |   html        |   true|false {false}
- * data-next        |   next        |   string|html depends on the data-html/html {'Next &#8618;'}
- * data-prev        |   prev        |   string|html depends on the data-html/html {'&#8617; Previous'}
- * data-overlay     |   overlay     |   true|false {true}
- *
- *
- * ### functions triggered
- * 1. onStart --> when the tour is start tour method is called.
- * 2. onClosed --> when the tour is close or destroyed. The current step element is available as a JQueryObject
- * 3. onChanged  --> when our tour moved from one step to another. The current step element is available as a JQueryObject
- * 4. onFinished(No use for the now) --> when our tour has finished its processing. The current step element is available as a JQueryObject
- * 
- * ### api stuff to build your custom view
- * 1. close
- * 2. next
- * 3. prev
- * 4. title
- * 5. content
- * 6. info
- * 
- * ### css for custom tour element stuff
- */
 var ProductTour;
-(function (jQuery) {
+(function(jQuery) {
     /**
      * Productor Class object
      * @param options holds the possible set of options allowed
      * @constructor
      */
-    ProductTour = function (options) {
-        jQuery("").click(function () {
-            
-        });
-        jQuery("").click(function () {
-            
-        });
-        jQuery("").click(function () {
-            
-        });
+    ProductTour = function(options) {
+        var _onStart = undefined;
+        var _onClose = undefined;
+        var _onChange = undefined;
+
+        var _options = {//default options
+            overlay: true,
+            template: 'default',
+            nextText: 'Next',
+            prevText: 'Prev',
+            contentStyle: 'text'
+        };
+
+        jQuery.extend(_options, options);
+
+        //validate _options after merging
+
+        /**
+         * set onStart callback
+         */
+        this.onStart = function(cb){
+            if(cb !== undefined && typeof cb === 'function') _onStart = cb;
+        }
+        /**
+         * set onClose callback
+         */
+        this.onClose = function(cb){
+            if(cb !== undefined && typeof cb === 'function') _onClose = cb;
+        }
+        /**
+         * set onChange callback
+         */
+        this.onChange = function(cb){
+            if(cb !== undefined && typeof cb === 'function') _onChange = cb;
+        }
+        /**
+         * function that register click listeners for 
+         * @close, @next @previous
+         */
+        function registerListeners() {
+            jQuery(".product-tour-wrapper .product-tour-close").click(function(e) {
+
+            });
+            jQuery(".product-tour-wrapper .product-tour-next").click(function(e) {
+
+            });
+            jQuery(".product-tour-wrapper .product-tour-prev").click(function(e) {
+
+            });
+        }
+
+        // if(_onClose !== undefined && typeof _onClose === 'function') _onClose(e);
     }
 } (jQuery));
